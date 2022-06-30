d0fc1bb07db5fd27c1ed831ae581620ce7aa5cac
BOSTONE069 <bostoneochieng69@gmail.com>
2022-06-30 16:20:44 -0400

    rogram that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from add_0 import add
 
+    from add_0 import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
 

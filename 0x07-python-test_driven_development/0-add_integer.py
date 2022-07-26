c573c3f15a1c335a3e09800e03854b4f50da7d17
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-26 11:30:59 -0400

    function that adds 2 integers.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,7 +5,7 @@
 
 def add_integer(a, b=98):
     """Return the integer addition of a and b.
-    Float arguments are typecasted to ints before addition is performed.
+    Float arguments are typecast to ints before addition is performed.
     Raises:
         TypeError: If either of a or b is a non-integer and non-float.
     """

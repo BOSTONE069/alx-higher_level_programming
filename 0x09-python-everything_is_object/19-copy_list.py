b79c5e1f2b9781492d5d483f30f32b0850546d5f
BOSTONE069 <bostoneochieng69@gmail.com>
2022-07-26 14:46:58 -0400

    Python - Everything is object.

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+
 def copy_list(l):
+    """
+    It returns a copy of the list that is passed in
+
+    :param l: The list to copy
+    :return: A copy of the list.
+    """
     return l.copy()

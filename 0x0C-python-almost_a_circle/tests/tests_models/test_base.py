a69cd915fb52af8c52fdb84f159245615665636f
BOSTONE069 <bostoneochieng69@gmail.com>
2022-08-08 18:40:19 -0400

    Python unit tests files

--- 0x0C-python-almost_a_circle/tests/tests_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/tests_models/test_base.py
@@ -16,7 +16,7 @@ import os
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
-from models.square import Square
+from models
 
 
 class TestBase_instantiation(unittest.TestCase):

1836adacce3271b7df9ade5412c77f4f072e6299
BOSTONE069 <bostoneochieng69@gmail.com>
2022-08-08 18:21:43 -0400

    Python unit tests files

---
+++ 0x0C-python-almost_a_circle/101-main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 101-main """
+from base import Base
+from rectangle import Rectangle
+from square import Square
+
+if __name__ == "__main__":
+
+    list_rectangles = [Rectangle(100, 40), Rectangle(90, 110, 30, 10), Rectangle(20, 25, 110, 80)]
+    list_squares = [Square(35), Square(15, 70, 50), Square(80, 30, 70)]
+
+    Base.draw(list_rectangles, list_squares)

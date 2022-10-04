916d8d5676a070065a9705987bed51c57a55f683
BOSTONE069 <bostoneochieng69@gmail.com>
2022-10-04 15:14:38 -0400

    function that returns the reversed version of a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-exports.esrever = function (list){
+exports.esrever = function (list) {
   const listCopy = [];
   list.forEach(element => listCopy.unshift(element));
   return listCopy;
 };
+

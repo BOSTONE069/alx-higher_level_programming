dccfb7b1057670abc5882ab773e5e6358c47a64b
BOSTONE069 <bostoneochieng69@gmail.com>
2022-10-09 15:18:25 -0400

    a script 14-model_city_fetch_by_state.py that prints all City objects from the database hbtn_0e_14_usa

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-"""Definition of the State class"""
-
+"""script for using sqlalchemy to model our models using ORM
+"""
 from sqlalchemy import Column, Integer, String, ForeignKey
+
 from model_state import Base
 
-class City(Base):
-    """Class State"""
 
+class City(Base):
+    """cities class for use with sqlalchemy
+        -> inherits from sqlalchemy declarative_base
+    """
     __tablename__ = 'cities'
-    id = Column(Integer, autoincrement=True,
-                primary_key=True, nullable=False, unique=True)
+
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, ForeignKey("state.id"))
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)

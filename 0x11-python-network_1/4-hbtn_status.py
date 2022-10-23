ec97f7c5cb0f1b792825f73661d202ed3b6de05b
BOSTONE069 <bostoneochieng69@gmail.com>
2022-10-23 13:52:40 -0400

    Fetches for url

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""python script for fetching data in url"""
+"""fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
-if __name__ == '__main__':
-    request = requests.get('https://alx-intranet.hbtn.io/status')
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
-    print(f"\t- type: {request.text}")
-    print(f"\t- content: {request.text}")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))

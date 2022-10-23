c80875ade9acef58f15e5e2b941543141931c3f5
BOSTONE069 <bostoneochieng69@gmail.com>
2022-10-23 13:34:05 -0400

    script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8).

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
 """script that takes in a URL,
 sends a request to the URL and
-displays the body of the
-response (decoded in utf-8).
+displays the body of the response (decoded in utf-8).
 """
 import urllib.request
 import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    req = urllib.request.Request(url)
+    request = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(req) as response:
+        with urllib.request.urlopen(request) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
-        print(f"Error code:{e.code}")
+        print('Error code:', e.code)

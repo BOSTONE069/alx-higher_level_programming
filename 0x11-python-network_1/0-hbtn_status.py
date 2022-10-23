6c8463464e5dbd8e1ceaf435f4d4968084017f55
BOSTONE069 <bostoneochieng69@gmail.com>
2022-10-23 14:33:46 -0400

    Python script that fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""python script for fetching data in url"""
-import urllib.request
+"""Fetches data from given URL"""
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    import urllib.request
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
             as response:
-        alx = response.read()
+        html = response.read()
 
         print('Body response:')
-        print(f'\t- type: {type(alx)}')
-        print(f'\t- content-type: {alx}')
-        print(f'\t- utf8 content: {alx.decode("utf-8", "replace")}')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))

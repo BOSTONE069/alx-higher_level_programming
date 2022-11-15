53e43c30600a3e99268a7507e6f7d2a2d7c1b950
BOSTONE069 <bostoneochieng69@gmail.com>
2022-11-15 16:15:18 -0500

    script that prints all characters of a Star Wars movie.

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,14 +1,28 @@
 #!/usr/bin/node
-
-const myArray = process.argv.slice(2);
 const request = require('request');
+const { argv } = require('process');
 
-if (Number.isInteger(parseInt(myArray[0]))) {
-  request('http://swapi.co/api/films/' + myArray[0], function (error, response, body) {
-    if (error) console.error('error:', error);
-    JSON.parse(body).characters.forEach(link => request(link, (error, response, body) => {
-      if (error) console.error('error:', error);
-      console.log(JSON.parse(body).name);
-    }));
+const BaseUrl = 'https://swapi-api.hbtn.io/api/films/';
+function MakeRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        resolve(body);
+      } else {
+        reject(error);
+      }
+    });
   });
 }
+
+async function main () {
+  const movie = await MakeRequest(BaseUrl + argv[2]);
+  const characters = JSON.parse(movie).characters;
+  characters.forEach(async function (element) {
+    const character = await MakeRequest(element);
+    const CharacterName = JSON.parse(character).name;
+    console.log(CharacterName);
+  });
+}
+
+main();

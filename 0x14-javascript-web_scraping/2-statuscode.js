068023834f84d71070b538030ba37fd428883813
BOSTONE069 <bostoneochieng69@gmail.com>
2022-11-15 12:42:05 -0500

    script that display the status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-request.get(process.argv[2]);
-request.on('response', function (response) {
+request.get(process.argv[2]).on('response', function (response) {
   console.log(`code: ${response.statusCode}`);
 });

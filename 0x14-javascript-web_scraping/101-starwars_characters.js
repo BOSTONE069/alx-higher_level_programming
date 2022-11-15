874df142fc00453596449b0450d79c779893ce2c
BOSTONE069 <bostoneochieng69@gmail.com>
2022-11-15 16:11:01 -0500

    script that prints all characters of a Star Wars movie.

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,23 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
+const myArray = process.argv.slice(2);
+const request = require('request');
+
+if (Number.isInteger(parseInt(myArray[0]))) {
+  request('http://swapi.co/api/films/' + myArray[0], function (error, response, body) {
+    if (error) console.error('error:', error);
+    const setTitle = JSON.parse(body).title;
+    if (Number.isInteger(parseInt(myArray[0]))) {
+      request('http://swapi.co/api/films/', function (error, response, body) {
+        if (error) console.error('error:', error);
+        JSON.parse(body).results.forEach(dict => {
+          if (dict.title === setTitle) {
+            dict.characters.forEach(link => request(link, (error, response, body) => {
+              if (error) console.error('error:', error); console.log(JSON.parse(body).name);
+            }));
+          }
+        });
+      });
     }
   });
 }
